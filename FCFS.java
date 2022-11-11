7fe86502e5c28165f8acaf90cfef356ce5a020ca
Marcosfr98 <mffdfreitas@gmail.com>
2022-11-10 21:39:00 -0300

    Término do algoritmo FCFS

--- FCFS.java
+++ FCFS.java
@@ -9,22 +9,29 @@ public class FCFS {
         System.out.print("Digite o número de processos: ");
         Integer numero_processos = entrada.nextInt();
         entrada.nextLine();
+
         Integer[] tempo_execucao = new Integer[numero_processos];
         Integer[] tempo_chegada = new Integer[numero_processos];
         Integer[] tempo_espera = new Integer[numero_processos];
+        Integer[] tempo_restante = new Integer[numero_processos];
+        Integer processo_em_execucao = 0;
+        Integer processos_terminados = 0;
+
         System.out.println("---------- Opções ----------");
         System.out.println("1) Atribuição automática");
         System.out.println("2) Atribuição manual");
+
         System.out.print("OPÇÃO: ");
         Integer atribuicao = entrada.nextInt();
         entrada.nextLine();
+
         if(atribuicao.equals(1)){
             //Atribuição automática
         }else if(atribuicao.equals(2)){
             for(Integer i = 0; i < numero_processos; i++) {
-                System.out.print("Digite o tempo de execução do processo (p" + i + 1 + "): ");
+                System.out.print("Digite o tempo de execução do processo (p" + (i + 1) + "): ");
                 tempo_execucao[i] = entrada.nextInt();
-
+                tempo_restante[i] = tempo_execucao[i];
             }
         }
         System.out.println("---------- ESCOLHA O TIPO DE ALGORITMO ----------");
@@ -33,19 +40,26 @@ public class FCFS {
         System.out.println("3) SJF Premptivo");
         System.out.print("OPÇÃO: ");
         Integer tipo_algoritmo = entrada.nextInt();
-        if(tipo_algoritmo.equals(1)){
-            Integer total_execucao = 0;
-            for(Integer i = 0; i < numero_processos; i++){
-                total_execucao += tempo_execucao[i];
-            }
+        if(tipo_algoritmo.equals(1)) {
             //FCFS
-            for(Integer i = 0; i < total_execucao; i++){
-                tempo_chegada[i] = i;
-                if(i == tempo_execucao[i]){
-                    tempo_chegada[i] = i;
-                }
-                System.out.print("ID: p " + i + 1 + " EXECUÇÃO: " + tempo_execucao[i] + " ESPERA: " + tempo_chegada[i]);
+
+            for (Integer i = 0; i < 1000; i++){
+                if(tempo_restante[processo_em_execucao] != 0) {
+                        System.out.println(i + "-" + (processo_em_execucao+1));
+                    tempo_restante[processo_em_execucao]--;
+                        if(tempo_restante[processo_em_execucao] == 0) {
+                            processos_terminados++;
+
+                            if(processos_terminados == numero_processos)
+                                break;
+                            else
+                                processo_em_execucao++;
+                        }
+                    }
+
+
             }
+
         }else if(tipo_algoritmo.equals(2)){
             //SJF Não-Premptivo
         }else if(tipo_algoritmo.equals(3)){
